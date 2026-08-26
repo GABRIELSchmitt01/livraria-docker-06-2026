42b439b924d997f48a1eb310cdfa7a8f18840673
Gabriel Schmitt <gabrielschmittmaciel2005@gmail.com>
2026-08-25 21:39:20 -0300

    First Commit

---
+++ app/main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+async def home():
+    return{"message": "Hello, World!"}
+
+@app.get("/saudacao")
+async def saudacao(string: str):
+    return{"message": f"Ola, {string}! Seja bem-vindo!"}
